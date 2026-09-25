1b14f85ce8c1b11ac7f2af7eda4f5bf98653eac9
radenmasabdul <abdul.tamsis@gmail.com>
2026-09-25 17:08:18 +0700

    feat(entity): add user entity

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter 
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  private String id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Status status;
+
+  private String phoneNumber;
+  private String profileImageUrl;
+  private Instant createdAt;
+  private Instant updatedAt;
+
+  public User() {
+  }
+}
